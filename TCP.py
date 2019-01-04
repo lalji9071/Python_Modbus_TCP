9a8121d9d994e0e543407c87ed14ec873cf6ec21
lalji9071 <46167129+lalji9071@users.noreply.github.com>
2019-01-04 13:03:12 +0100

    Update TCP.py

--- TCP.py
+++ TCP.py
@@ -16,16 +16,25 @@ c.host("192.168.58.10")
 c.port(502)
 # managing TCP sessions with call to c.open()/c.close()
 c.open()
+
+# to debug the communication
+#c.debug(True)
+inputRegD = c.read_discrete_inputs(0, 16)
+coil = c.read_coils(0, 16)
+read_holding = c.read_holding_registers(0, 2)
+inputReg = c.read_input_registers(0, 2)
+
 while True:
-    regs = c.read_discrete_inputs(0, 16)
-    coil= c.read_coils(0,16)
-    if regs:
+    if inputRegD:
         #print(regs)
         #print(type(regs))
-        #print(coil)
         #print(type(coil))
-        x=regs[5]
-        print(x)
+        x=inputRegD[5]
+        print("Read Input: " + str(inputRegD))
+        print("Read Coils: " + str(coil))
+        print("Holding Register: "+ str(read_holding))
+        print("Read Input Register: " + str(inputReg))
+
     else:
         print("read error")
     time.sleep(5)
